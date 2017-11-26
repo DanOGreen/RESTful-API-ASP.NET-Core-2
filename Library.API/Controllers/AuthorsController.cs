c304fbfa0191435d3f227fa29636f9fa48cfc2a2
Daniel Green <dan.oryan.green@gmail.com>
2017-11-25 23:35:49 -0500

    project setup

---
+++ Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Library.Service.DataAccess;
+
+namespace Library.API.Controllers
+{
+    [Route("api / authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository libaryRepository;
+
+        public AuthorsController(ILibraryRepository libaryRepository)
+        {
+            this.libaryRepository = libaryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = libaryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
